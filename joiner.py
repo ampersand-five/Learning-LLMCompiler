267665c59ac09b162c1da4726b8f8c1afaab8222
Data Eng <9754171+ampersand-five@users.noreply.github.com>
2024-03-27 16:45:55 -0500

    Add joiner

---
+++ joiner.py
@@ -0,0 +1,79 @@
+from typing import Sequence, Any, Union, Iterable, List, Tuple, Dict
+
+from langchain_core.language_models import BaseChatModel
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableBranch
+from langchain_core.tools import BaseTool
+from langchain_core.messages import (
+    BaseMessage,
+    FunctionMessage,
+    HumanMessage,
+    SystemMessage,
+)
+
+from output_parser import LLMCompilerPlanParser, Task
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain.chains.openai_functions import create_structured_output_runnable
+from langchain_core.messages import AIMessage
+
+from task_fetching_unit import tool_messages
+
+
+
+class FinalResponse(BaseModel):
+    """The final response/answer."""
+
+    response: str
+
+
+class Replan(BaseModel):
+    feedback: str = Field(
+        description="Analysis of the previous attempts and recommendations on what needs to be fixed."
+    )
+
+
+class JoinOutputs(BaseModel):
+    """Decide whether to replan or whether you can return the final response."""
+
+    thought: str = Field(
+        description="The chain of thought reasoning for the selected action"
+    )
+    action: Union[FinalResponse, Replan]
+
+
+joiner_prompt = hub.pull("wfh/llm-compiler-joiner").partial(
+    examples=""
+)  # You can optionally add examples
+llm = ChatOpenAI(model="gpt-4-turbo-preview")
+
+runnable = create_structured_output_runnable(JoinOutputs, llm, joiner_prompt)
+
+def _parse_joiner_output(decision: JoinOutputs) -> List[BaseMessage]:
+    response = [AIMessage(content=f"Thought: {decision.thought}")]
+    if isinstance(decision.action, Replan):
+        return response + [
+            SystemMessage(
+                content=f"Context from last attempt: {decision.action.feedback}"
+            )
+        ]
+    else:
+        return response + [AIMessage(content=decision.action.response)]
+
+
+def select_recent_messages(messages: list) -> dict:
+    selected = []
+    for msg in messages[::-1]:
+        selected.append(msg)
+        if isinstance(msg, HumanMessage):
+            break
+    return {"messages": selected[::-1]}
+
+
+joiner = select_recent_messages | runnable | _parse_joiner_output
+
+# Example usage
+example_question = "What's the temperature in SF raised to the 3rd power?"
+input_messages = [HumanMessage(content=example_question)] + tool_messages
+joiner.invoke(input_messages)
