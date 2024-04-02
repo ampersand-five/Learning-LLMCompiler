e92d7293b2eb0569435a71c9715b1983dfb3be52
Data Eng <9754171+ampersand-five@users.noreply.github.com>
2024-04-02 14:46:44 -0500

    Change search results to return 2 results.

--- compiler_agent/utils/tools.py
+++ compiler_agent/utils/tools.py
@@ -1,10 +1,8 @@
 import json
 from langchain_openai import ChatOpenAI
-from langchain_community.tools.tavily_search import TavilySearchResults
 
-from langchain_openai import ChatOpenAI
+# Tool imports
 from langchain_community.tools.tavily_search import TavilySearchResults
-
 from utils.math_tools import get_math_tool
 
 
@@ -14,7 +12,13 @@ with open('compiler_agent/config.json', 'r') as f:
 
 calculate = get_math_tool(ChatOpenAI(**config['math_llm']))
 search = TavilySearchResults(
-  max_results=1,
+  # Setting results to 2 for: shows possible return values from tools, they can be an
+  # array, a string, etc. For this tool it's an list of dicts. As a learning/tutorial,
+  # this will help illustrate better than 1, that the results are per tool and not
+  # standarized. Also not higher than 2 because costs for a free tutorial, keeping it
+  # low for that.
+  max_results=2,
+  # Setting a different description here than the default one.
   description='tavily_search_results_json(query="the search query") - a search engine.',
 )
 
